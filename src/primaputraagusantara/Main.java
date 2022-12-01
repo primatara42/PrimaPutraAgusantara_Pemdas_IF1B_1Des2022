c866006d16240d2044cac4d08c0e0b2698d17292
Prima <primatara42@gmail.com>
2022-12-01 11:56:33 +0700

    Update Method Luas Segitiga

---
+++ src/primaputraagusantara/Main.java
@@ -0,0 +1,36 @@
+package primaputraagusantara;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Main
+{
+    static Scanner input = new Scanner(System.in);
+
+    //method tanpa nilai balik dan tanpa parameter
+    public static void hitungLuasSegitiga()
+    {
+        int alas, tinggi;
+        double luas;
+
+        System.out.print("Masukkan Alas = ");
+        alas = input.nextInt();
+
+        System.out.print("Masukkan Tinggi = ");
+        tinggi = input.nextInt();
+        
+        //PROSES
+        luas = (double) (alas * tinggi) / 2;
+
+        //OUTPUT
+        System.out.println("Luas Segitiga adalah " + luas + " cm");
+    }
+
+    public static void main(String[] args) //method utama
+    {
+        hitungLuasSegitiga();
+
+    }
+
+
+}
